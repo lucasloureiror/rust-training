28b8b3546e0896d0a43e2461c29544f53e5e8630
Lucas <prolucasloureiro@gmail.com>
2022-10-10 16:39:29 -0300

    Comecei a bit array em rust

---
+++ Estrutura de Dados/1. Conjunto (bit array)/Estático/main.rs
@@ -0,0 +1,29 @@
+use std::io;
+fn main(){
+
+    let mut vetor: [usize; 100] = [0;100];
+
+   
+    
+    
+
+    loop{
+        println!("Digite o numero que deseja guardar na bit array ou digite Q para sair");
+        let mut input = String:: new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Falha na leitura");
+
+            match input == "Q"{
+                break;
+
+            }
+               let input: usize = input.trim().parse().expect("Nao digitou um numero!");
+               vetor[input - 1] = 1;
+
+            }
+    }
+    
+    
+    
